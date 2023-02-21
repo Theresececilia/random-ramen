74a8dc0f6430b2b52b903c1419f12b5ff730a1e8
Theresececilia <therese.johansson@edu.cmeducations.se>
2023-02-21 17:33:01 +0100

    add pages

--- random-ramen-recipe/src/components/Recipe.jsx
+++ random-ramen-recipe/src/components/Recipe.jsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 const ramenRecipes = [
   {
     title: "Miso Ramen",
-    ingredients: ["ramen noodles", "miso paste", "soy sauce", "chicken broth", "pork belly", "green onions"],
+    ingredients: ["ramen noodles,", "miso paste,", "soy sauce,", "chicken broth,", "pork belly,", "green onions"],
     instructions1: "1. Cook the ramen noodles according to the package instructions."  ,
     instructions2: "2. In a separate pot, bring the chicken broth to a simmer.",
     instructions3: "3. Whisk in the miso paste and soy sauce until fully combined.",
@@ -14,7 +14,7 @@ const ramenRecipes = [
   },
   {
     title: "Tonkotsu Ramen",
-    ingredients: ["ramen noodles", "pork bones", "soy sauce", "mirin", "sesame oil", "green onions"],
+    ingredients: ["ramen noodles,", "pork bones,", "soy sauce,", "mirin,", "sesame oil,", "green onions"],
     instructions1: "1. In a large pot, add the pork bones and enough water to cover them. Bring to a boil and then reduce to a simmer for 12-24 hours. ",
     instructions2: "2. Strain the broth and return it to the pot.",  
     instructions3: "3. Add soy sauce, mirin, and sesame oil to the broth and bring it to a simmer.",  
@@ -23,7 +23,7 @@ const ramenRecipes = [
   },
   {
     title: "Shoyu Ramen",
-    ingredients: ["ramen noodles", "soy sauce", "chicken broth", "mirin", "pork belly", "bamboo shoots"],
+    ingredients: ["ramen noodles,", "soy sauce,", "chicken broth,", "mirin,", "pork belly,", "bamboo shoots"],
     instructions1: "1. Cook the ramen noodles according to the package instructions.",
     instructions2: "2. In a separate pot, bring the chicken broth to a simmer.",  
     instructions3: "3. Add soy sauce and mirin to the broth and stir to combine.",  
@@ -32,7 +32,7 @@ const ramenRecipes = [
   },
   {
     title: "Vegetarian Ramen",
-    ingredients: ["ramen noodles", "mushrooms", "soy sauce", "vegetable broth", "tofu", "green onions"],
+    ingredients: ["ramen noodles,", "mushrooms,", "soy sauce,", "vegetable broth,", "tofu,", "green onions"],
     instructions1: "1. Cook the ramen noodles according to the package instructions.",
     instructions2: "2. In a separate pot, bring the vegetable broth to a simmer.",  
     instructions3: "3. Add sliced mushrooms and soy sauce to the broth and let simmer for 10-15 minutes.",  
@@ -41,7 +41,7 @@ const ramenRecipes = [
   },
   {
     title: "Curry Ramen",
-    ingredients: ["ramen noodles", "curry powder", "coconut milk", "chicken broth", "chicken thighs", "cilantro"],
+    ingredients: ["ramen noodles,", "curry powder,", "coconut milk,", "chicken broth,", "chicken thighs,", "cilantro"],
     instructions1: "1. Cook the ramen noodles according to the package instructions."    ,
     instructions2: "2. In a separate pot, brown the chicken thighs on both sides.",  
     instructions3: "3. Add chicken broth, curry powder, and coconut milk to the pot and bring to a simmer.",  
@@ -73,7 +73,7 @@ const RandomRamenRecipe = () => {
           <li className={classes.listItems} key={index}>{ingredient}</li>
         ))}
       </ul>
-      <h2>Instructions:</h2>
+      <h2 className={classes.instructions} >Instructions:</h2>
       <p>{currentRecipe.instructions1}</p>
       <p>{currentRecipe.instructions2}</p>
       <p>{currentRecipe.instructions3}</p>
