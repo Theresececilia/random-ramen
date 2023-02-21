f3c01139831b06ecd74bac991fb67adfde55051e
Theresececilia <therese.johansson@edu.cmeducations.se>
2023-02-21 15:58:25 +0100

    add big device styles

--- random-ramen-recipe/src/components/Navbar.jsx
+++ random-ramen-recipe/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div className={classes.navbar}>
       <div className={classes.innerNav}>
-        <h3 className={classes.title}>RANDOM-RAMEN</h3>
+        <h3 className={classes.title}>RANDOMRAMEN</h3>
         <p className={classes.yum}>Yum Yum</p>
       </div>
       <div className={classes.innerNav}>
--- random-ramen-recipe/src/components/Recipe.jsx
+++ random-ramen-recipe/src/components/Recipe.jsx
@@ -6,27 +6,47 @@ const ramenRecipes = [
   {
     title: "Miso Ramen",
     ingredients: ["ramen noodles", "miso paste", "soy sauce", "chicken broth", "pork belly", "green onions"],
-    instructions: "1. Cook the ramen noodles according to the package instructions. 2. In a separate pot, bring the chicken broth to a simmer. 3. Whisk in the miso paste and soy sauce until fully combined. 4. Add the pork belly to the broth and let simmer for 10-15 minutes. 5. Serve the noodles in a bowl, pour the broth over them, and garnish with sliced green onions."
+    instructions1: "1. Cook the ramen noodles according to the package instructions."  ,
+    instructions2: "2. In a separate pot, bring the chicken broth to a simmer.",
+    instructions3: "3. Whisk in the miso paste and soy sauce until fully combined.",
+    instructions4: "4. Add the pork belly to the broth and let simmer for 10-15 minutes.",
+    instructions5: "5. Serve the noodles in a bowl, pour the broth over them, and garnish with sliced green onions."
   },
   {
     title: "Tonkotsu Ramen",
     ingredients: ["ramen noodles", "pork bones", "soy sauce", "mirin", "sesame oil", "green onions"],
-    instructions: "1. In a large pot, add the pork bones and enough water to cover them. Bring to a boil and then reduce to a simmer for 12-24 hours. 2. Strain the broth and return it to the pot. 3. Add soy sauce, mirin, and sesame oil to the broth and bring it to a simmer. 4. Cook the ramen noodles according to the package instructions. 5. Serve the noodles in a bowl, pour the broth over them, and garnish with sliced green onions."
+    instructions1: "1. In a large pot, add the pork bones and enough water to cover them. Bring to a boil and then reduce to a simmer for 12-24 hours. ",
+    instructions2: "2. Strain the broth and return it to the pot.",  
+    instructions3: "3. Add soy sauce, mirin, and sesame oil to the broth and bring it to a simmer.",  
+    instructions4: "4. Cook the ramen noodles according to the package instructions.",  
+    instructions5:  "5. Serve the noodles in a bowl, pour the broth over them, and garnish with sliced green onions.",  
   },
   {
     title: "Shoyu Ramen",
     ingredients: ["ramen noodles", "soy sauce", "chicken broth", "mirin", "pork belly", "bamboo shoots"],
-    instructions: "1. Cook the ramen noodles according to the package instructions. 2. In a separate pot, bring the chicken broth to a simmer. 3. Add soy sauce and mirin to the broth and stir to combine. 4. Add the pork belly to the broth and let simmer for 10-15 minutes. 5. Serve the noodles in a bowl, pour the broth over them, and garnish with sliced bamboo shoots."
+    instructions1: "1. Cook the ramen noodles according to the package instructions.",
+    instructions2: "2. In a separate pot, bring the chicken broth to a simmer.",  
+    instructions3: "3. Add soy sauce and mirin to the broth and stir to combine.",  
+    instructions4: "4. Add the pork belly to the broth and let simmer for 10-15 minutes.",  
+    instructions5: "5. Serve the noodles in a bowl, pour the broth over them, and garnish with sliced bamboo shoots.",  
   },
   {
     title: "Vegetarian Ramen",
     ingredients: ["ramen noodles", "mushrooms", "soy sauce", "vegetable broth", "tofu", "green onions"],
-    instructions: "1. Cook the ramen noodles according to the package instructions. 2. In a separate pot, bring the vegetable broth to a simmer. 3. Add sliced mushrooms and soy sauce to the broth and let simmer for 10-15 minutes. 4. Slice the tofu into small cubes and add it to the broth. 5. Serve the noodles in a bowl, pour the broth over them, and garnish with sliced green onions."
+    instructions1: "1. Cook the ramen noodles according to the package instructions.",
+    instructions2: "2. In a separate pot, bring the vegetable broth to a simmer.",  
+    instructions3: "3. Add sliced mushrooms and soy sauce to the broth and let simmer for 10-15 minutes.",  
+    instructions4: "4. Slice the tofu into small cubes and add it to the broth.",  
+    instructions5: "5. Serve the noodles in a bowl, pour the broth over them, and garnish with sliced green onions.",   
   },
   {
     title: "Curry Ramen",
     ingredients: ["ramen noodles", "curry powder", "coconut milk", "chicken broth", "chicken thighs", "cilantro"],
-    instructions: "1. Cook the ramen noodles according to the package instructions. 2. In a separate pot, brown the chicken thighs on both sides. 3. Add chicken broth, curry powder, and coconut milk to the pot and bring to a simmer. 4. Let simmer for 20-30 minutes until chicken is cooked through. 5. Serve the noodles in a bowl, pour the broth over them, and garnish with chopped cilantro."
+    instructions1: "1. Cook the ramen noodles according to the package instructions."    ,
+    instructions2: "2. In a separate pot, brown the chicken thighs on both sides.",  
+    instructions3: "3. Add chicken broth, curry powder, and coconut milk to the pot and bring to a simmer.",  
+    instructions4: "4. Let simmer for 20-30 minutes until chicken is cooked through.",  
+    instructions5: "5. Serve the noodles in a bowl, pour the broth over them, and garnish with chopped cilantro.",  
   }
   ]
 
@@ -44,16 +64,22 @@ const RandomRamenRecipe = () => {
   return (
     <>
     <div className={classes.container}>
-      <h1>{currentRecipe.title}</h1>
-      <h2>Ingredients:</h2>
-      <ul>
+      <div className={classes.innerContainer} >
+      <h2 className={classes.ingredients} >Ingredients:</h2>
+      <h1 className={classes.ramenTitle} >{currentRecipe.title}</h1>
+      </div>
+      <ul className={classes.list} >
         {currentRecipe.ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
+          <li className={classes.listItems} key={index}>{ingredient}</li>
         ))}
       </ul>
       <h2>Instructions:</h2>
-      <p>{currentRecipe.instructions}</p>
-      <button onClick={handleNewRecipeClick}>Get a new recipe</button>
+      <p>{currentRecipe.instructions1}</p>
+      <p>{currentRecipe.instructions2}</p>
+      <p>{currentRecipe.instructions3}</p>
+      <p>{currentRecipe.instructions4}</p>
+      <p>{currentRecipe.instructions5}</p>
+      <button className={classes.button} onClick={handleNewRecipeClick}>New recipe</button>
     </div>
     </>
   );
